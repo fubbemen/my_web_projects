533b4748e3139af4bd580a5080b4d1c05b7e4597
fubbemen <fabian@zinknet.se>
2025-12-23 22:59:59 +0100

    added one day forecast, fixed grammar and ui;)

--- weater_app/index.php
+++ weater_app/index.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" ){
     
     $city = $_POST["city"];
     $city = str_replace(" "," ",$city);  
-    $geo_url = "https://geocoding-api.open-meteo.com/v1/search?name=" . urlencode($city) . "&count=1";  // Added urlencode
+    $geo_url = "https://geocoding-api.open-meteo.com/v1/search?name=" . urlencode($city) . "&count=1";  
     $geo_response = file_get_contents( $geo_url );
     $geo_data = json_decode( $geo_response, true );
 
@@ -12,15 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" ){
             $lat = $geo_data["results"][0]["latitude"];
             $long = $geo_data["results"][0]["longitude"];
             
-            
             $url = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$long&current=temperature_2m,weather_code";
-
-            
             $response = file_get_contents($url);
-
-            
             $data = json_decode($response, true);
-
             
             $temp = $data['current']['temperature_2m'];
             $code = $data['current']['weather_code'];
@@ -42,6 +36,34 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" ){
                 $weather = "storm!";
             }
             
+            // TOMORROW'S FORECAST VARIABLES ADDED
+            $url_forecast = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$long&daily=temperature_2m_max,temperature_2m_min,weathercode&forecast_days=2";
+            $response_forecast = file_get_contents($url_forecast);
+            $data_forecast = json_decode($response_forecast, true);
+            $code_forecast = $data_forecast['daily']['weathercode'][1];
+            $temp_max = $data_forecast['daily']['temperature_2m_max'][1];
+            $temp_min = $data_forecast['daily']['temperature_2m_min'][1];
+            $avg_temp = ($temp_max + $temp_min) / 2;
+            
+            if ($code_forecast == 0) {
+                $weather_forecast = "klar himmel";
+            } else if (in_array($code_forecast,[1,2,3])) {
+                $weather_forecast = "molnigt";
+            } else if (in_array($code_forecast,[45,48])) {
+                $weather_forecast = "dimigt";
+            } else if (in_array($code_forecast,[51,53,55,61,63,65])) {
+                $weather_forecast = "regn";
+            } else if (in_array($code_forecast,[80,81,82])){
+                $weather_forecast = "regn skurar"; 
+            } else if (in_array($code_forecast,[85,86,71,73,75,77])){
+                $weather_forecast = "snöigt";
+            } else if (in_array($code_forecast,[95,96,99])){
+                $weather_forecast = "storm!";
+            } else {
+                $weather_forecast = "blandat väder";
+            }
+            // END OF ADDED VARIABLES
+            
             $cityFound = true;
         } else {
             if ($city != ""){  
@@ -56,31 +78,37 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" ){
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="/my_web_projects/weater_app/style.css">
     <title>Weather Forecast</title>
     <link rel="shortcut icon" type="image/x-icon" href="img.png" />
-    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Cherry Bomb One" rel="stylesheet">
-    
 </head>
 
 <body>
     <div class="weather-box">
-        <h1 class="weather-title">Weather Forecast</h1>
+        <h1 class="weather-title">världens typ bästa väder app</h1>
 
         <form method="POST">
             <div class="weather-row">
-                <input type="text" name="city" placeholder="skriv in staden du will veta vädret i">
+                <input class="input" type="text" name="city" placeholder="ange namnet på staden du vill se vädret på">
             </div>
 
             <div class="weather-row">
-                <button type="submit">sicka in för granskning av väder i valda staden</button>
+                <button type="submit">sicka in för granskning av vädret i valda staden</button>
             </div>
         </form>
 
         <?php
         if (isset($_POST['city']) && !empty($_POST['city']) && isset($cityFound) && $cityFound === true){
+            echo "<p>---------------------------------</p>";
+            echo "<p>det nuvarande vädret är förljande:<p>";
             echo "<p>tempraturen i $city är "."$temp"."℃</p>";
-            echo "<p>och vädret är $weather</p>";
+            echo "<p>och det är $weather</p>";
+            echo "<p>---------------------------------</p>";
+            echo "<p>morgondagens väder är följande:</p>";
+            echo "<p>medel tempraturen är"." $avg_temp" . "℃ </p>";
+            echo "<p>och vädret kommer att vara $weather_forecast</p>";
+            echo "<p>---------------------------------</p>";
         }
         ?>
     </div>
