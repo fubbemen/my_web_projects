c9832030224d279b5f0b7c77f101a31dcf72b767
fubbemen <fabian@zinknet.se>
2026-01-03 01:38:05 +0100

    maybe better

---
+++ weather/language.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>select</title>
+      <link rel="shortcut icon" type="image/x-icon" href="img.png" />
+    <link href="https://fonts.googleapis.com/css?family=Cherry Bomb One" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    
+
+<form method="POST">
+    <div class = "weather-box">
+    <select name="lang" id="lang" class = "inputs">
+  <option value="english">English</option>
+  <option value="中文">中文</option>
+  <option value="Español">Español</option>
+  <option value="हिन्दी">हिन्दी</option>
+  <option value="العربية">العربية</option>
+  <option value="বাংলা">বাংলা</option>
+  <option value="Português">Português</option>
+  <option value="Русский">Русский</option>
+  <option value="日本語">日本語</option>
+  <option value="ਪੰਜਾਬੀ">ਪੰਜਾਬੀ</option>
+  <option value="Deutsch">Deutsch</option>
+  <option value="Basa Jawa">Basa Jawa</option>
+  <option value="한국어">한국어</option>
+  <option value="Français">Français</option>
+  <option value="తెలుగు">తెలుగు</option>
+  <option value="मराठी">मराठी</option>
+  <option value="தமிழ்">தமிழ்</option>
+  <option value="اردو">اردو</option>
+  <option value="Türkçe">Türkçe</option>
+  <option value="Italiano">Italiano</option>
+  <option value="ไทย">ไทย</option>
+  <option value="ગુજરાતી">ગુજરાતી</option>
+  <option value="فارسی">فارسی</option>
+  <option value="Polski">Polski</option>
+  <option value="Українська">Українська</option>
+  <option value="മലയാളം">മലയാളം</option>
+  <option value="Basa Sunda">Basa Sunda</option>
+  <option value="ଓଡ଼ିଆ">ଓଡ଼ିଆ</option>
+  <option value="ಕನ್ನಡ">ಕನ್ನಡ</option>
+  <option value="Română">Română</option>
+  <option value="Hausa">Hausa</option>
+  <option value="မြန်မာစာ">မြန်မာစာ</option>
+  <option value="客家話">客家話</option>
+  <option value="Ελληνικά">Ελληνικά</option>
+  <option value="Čeština">Čeština</option>
+  <option value="Eesti">Eesti</option>
+  <option value="Azərbaycan dili">Azərbaycan dili</option>
+  <option value="Nederlands">Nederlands</option>
+  <option value="አማርኛ">አማርኛ</option>
+  <option value="Fulfulde">Fulfulde</option>
+  <option value="Hrvatski">Hrvatski</option>
+  <option value="Беларуская">Беларуская</option>
+  <option value="Magyar">Magyar</option>
+  <option value="עברית">עברית</option>
+  <option value="ትግርኛ">ትግርኛ</option>
+  <option value="ລາວ">ລາວ</option>
+  <option value="සිංහල">සිංහල</option>
+  <option value="ភាសាខ្មែរ">ភាសាខ្មែរ</option>
+  <option value="Norsk">Norsk</option>
+  <option value="Oʻzbek">Oʻzbek</option>
+  <option value="Bahasa Melayu">Bahasa Melayu</option>
+  <option value="پښتو">پښتو</option>
+  <option value="Suomi">Suomi</option>
+  <option value="Català">Català</option>
+  <option value="Български">Български</option>
+  <option value="Српски">Српски</option>
+  <option value="ქართული">ქართული</option>
+  <option value="Монгол">Монгол</option>
+  <option value="Kiswahili">Kiswahili</option>
+  <option value="Afrikaans">Afrikaans</option>
+  <option value="Tiếng Việt">Tiếng Việt</option>
+  <option value="Shqip">Shqip</option>
+  <option value="Кыргызча">Кыргызча</option>
+  <option value="Հայերեն">Հայերեն</option>
+  <option value="Igbo">Igbo</option>
+  <option value="မြန်မာဘာသာ">မြန်မာဘာသာ</option>
+  <option value="Bamanankan">Bamanankan</option>
+  <option value="Sesotho">Sesotho</option>
+  <option value="Kisii">Kisii</option>
+  <option value="Lingála">Lingála</option>
+  <option value="Shona">Shona</option>
+  <option value="Nyanja">Nyanja</option>
+  <option value="Xitsonga">Xitsonga</option>
+  <option value="Xitsonga">Xitsonga</option>
+  <option value="Twi">Twi</option>
+  <option value="Zulu">Zulu</option>
+  <option value="Xhosa">Xhosa</option>
+  <option value="Hmong">Hmong</option>
+  <option value="Bislama">Bislama</option>
+  <option value="Chichewa">Chichewa</option>
+  <option value="Kikuyu">Kikuyu</option>
+  <option value="Kinyarwanda">Kinyarwanda</option>
+  <option value="Tonga">Tonga</option>
+  <option value="Tongan">Tongan</option>
+  <option value="Fijian">Fijian</option>
+  <option value="Samoan">Samoan</option>
+  <option value="Tahitian">Tahitian</option>
+  <option value="Hiri Motu">Hiri Motu</option>
+  <option value="Tok Pisin">Tok Pisin</option>
+  <option value="Chamorro">Chamorro</option>
+  <option value="Guarani">Guarani</option>
+  <option value="Quechua">Quechua</option>
+  <option value="Aymara">Aymara</option>
+  <option value="Nahuatl">Nahuatl</option>
+  <option value="Māori">Māori</option>
+  <option value="Sámi">Sámi</option>
+  <option value="Inuktitut">Inuktitut</option>
+  <option value="Kalaallisut">Kalaallisut</option>
+  <option value="Tlingit">Tlingit</option>
+  <option value="Cherokee">ᏣᎳᎩ</option>
+  <option value="Navajo">Diné bizaad</option>
+  <option value="Mohawk">Kanienʼkéha</option>
+  <option value="Ojibwe">Anishinaabemowin</option>
+  <option value="Cree">ᓀᐦᐃᔭᐤ</option>
+  <option value="Inupiaq">Iñupiaq</option>
+  <option value="Sicilianu">Sicilianu</option>
+  <option value="Català">Català</option>
+  <option value="Aragonés">Aragonés</option>
+  <option value="Asturianu">Asturianu</option>
+  <option value="Occitan">Occitan</option>
+  <option value="Brezhoneg">Brezhoneg</option>
+  <option value="Français">Français</option>
+  <option value="Corsu">Corsu</option>
+  <option value="Gaeilge">Gaeilge</option>
+  <option value="Gaelg">Gaelg</option>
+  <option value="Gàidhlig">Gàidhlig</option>
+  <option value="Manx">Gaelg</option>
+  <option value="Føroyskt">Føroyskt</option>
+  <option value="Isländska">Íslenska</option>
+  <option value="Lëtzebuergesch">Lëtzebuergesch</option>
+  <option value="Malti">Malti</option>
+  <option value="Norsk">Norsk</option>
+  <option value="Nedersaksisch">Nedersaksisch</option>
+  <option value="Frysk">Frysk</option>
+  <option value="Limburgs">Limburgs</option>
+  <option value="Română">Română</option>
+  <option value="Magyar">Magyar</option>
+  <option value="Slovenščina">Slovenščina</option>
+  <option value="Slovenski">Slovenski</option>
+  <option value="Hrvatski">Hrvatski</option>
+  <option value="Bosanski">Bosanski</option>
+  <option value="Srpski">Srpski</option>
+  <option value="Česky">Česky</option>
+  <option value="Slovensko">Slovensko</option>
+  <option value="Polski">Polski</option>
+  <option value="Deutsch">Deutsch</option>
+  <option value="Nederlands">Nederlands</option>
+  <option value="Français">Français</option>
+  <option value="Español">Español</option>
+  <option value="Italiano">Italiano</option>
+  <option value="Português">Português</option>
+  <option value="English">English</option>
+  <option value="Dansk">Dansk</option>
+  <option value="Suomi">Suomi</option>
+  <option value="Íslenska">Íslenska</option>
+  <option value="Eesti">Eesti</option>
+  <option value="Latviešu">Latviešu</option>
+  <option value="Lietuvių">Lietuvių</option>
+  <option value="Māori">Māori</option>
+  <option value="Türkçe">Türkçe</option>
+  <option value="Kurdî">Kurdî</option>
+  <option value="العربية">العربية</option>
+  <option value="فارسی">فارسی</option>
+  <option value="தமிழ்">தமிழ்</option>
+  <option value="తెలుగు">తెలుగు</option>
+  <option value="हिन्दी">हिन्दी</option>
+  <option value="বাংলা">বাংলা</option>
+  <option value="ਪੰਜਾਬੀ">ਪੰਜਾਬੀ</option>
+  <option value="ગુજરાતી">ગુજરાતી</option>
+  <option value="मराठी">मराठी</option>
+  <option value="اردو">اردو</option>
+  <option value="ภาษาไทย">ภาษาไทย</option>
+  <option value="ລາວ">ລາວ</option>
+  <option value="မြန်မာ">မြန်မာ</option>
+  <option value="ខ្មែរ">ខ្មែរ</option>
+  <option value="አማርኛ">አማርኛ</option>
+  <option value="සිංහල">සිංහල</option>
+  <option value="עברית">עברית</option>
+  <option value="አማርኛ">አማርኛ</option>
+  <option value="ქართული">ქართული</option>
+  <option value="հայերեն">հայերեն</option>
+  <option value="Кыргызча">Кыргызча</option>
+  <option value="Монгол">Монгол</option>
+  <option value="ខ្មែរ">ខ្មែរ</option>
+  <option value="ગુજરાતી">ગુજરાતી</option>
+  <option value="संस्कृतम्">संस्कृतम्</option>
+  <option value="བོད་ཡིག">བོད་ཡིག</option>
+  <option value="አማርኛ">አማርኛ</option>
+  <option value="Tiếng Việt">Tiếng Việt</option>
+  <option value="Bamanankan">Bamanankan</option>
+  <option value="Kinyarwanda">Kinyarwanda</option>
+  <option value="Lingála">Lingála</option>
+  <option value="Shona">Shona</option>
+  <option value="Nyanja">Nyanja</option>
+  <option value="Kikuyu">Kikuyu</option>
+  <option value="Twi">Twi</option>
+  <option value="Zulu">Zulu</option>
+  <option value="Xhosa">Xhosa</option>
+  <option value="Sotho">Sesotho</option>
+  <option value="Xitsonga">Xitsonga</option>
+  <option value="Hmong">Hmong</option>
+  <option value="Chichewa">Chichewa</option>
+  <option value="Bislama">Bislama</option>
+  <option value="Tok Pisin">Tok Pisin</option>
+  <option value="Hiri Motu">Hiri Motu</option>
+  <option value="Tongan">Tongan</option>
+  <option value="Samoan">Samoan</option>
+  <option value="Fijian">Fijian</option>
+  <option value="Tahiti">Tahitian</option>
+  <option value="Chamorro">Chamorro</option>
+  <option value="Guaraní">Guaraní</option>
+  <option value="Quechua">Quechua</option>
+  <option value="Aymara">Aymara</option>
+  <option value="Nahuatl">Nahuatl</option>
+  <option value="Inuktitut">Inuktitut</option>
+  <option value="Kalaallisut">Kalaallisut</option>
+  <option value="Tlingit">Tlingit</option>
+  <option value="ᏣᎳᎩ">ᏣᎳᎩ</option>
+  <option value="Diné bizaad">Diné bizaad</option>
+  <option value="Kanienʼkéha">Kanienʼkéha</option>
+  <option value="Anishinaabemowin">Anishinaabemowin</option>
+  <option value="ᓀᐦᐃᔭᐤ">ᓀᐦᐃᔭᐤ</option>
+  <option value="Iñupiaq">Iñupiaq</option>
+  <option value="Sicilianu">Sicilianu</option>
+</select>
+<div>
+    <button type = "submit">sicka in</button>
+</div>
+
+<?php
+$lang = $_POST["lang"] ?? '';
+if ($lang != "")
+{
+    echo "<select name = 'lang' id = 'lang'>
+    </select>";
+    echo"<p>oj då $lang finns ej i vår språkdatabas en kontakta gärna support om detta för att få detta språk att funka!</p>";
+    echo"<p>mail: skitpådig@ingenbryrsig.se</p>";
+    echo "<p>telefon: 070 123 565 657 234(12)</p>";
+    echo "<p>brev: skitpådigvägen 67</p>";
+} else
+{
+    echo "ange ett språk";
+}
+
+?>
+
+
+</form>
+<div>
+<button onclick="document.location='index.php'">Gå tillbaka</button>
+</div>
+</div>
+</body>
+</html>
